51e803a9cecbf6bae8384185f55a075612680e39
manhtdd <manh.td120901@gmail.com>
2024-05-27 11:22:57 +0700

    Update main

--- src/main.py
+++ src/main.py
@@ -1,16 +1,19 @@
 from .module1.example1 import example1
 from .module2.example2 import example2
 from .utils.logger import logger
-import cProfile, pstats, io
+import cProfile, pstats, io, os
 
 def main():
     # Your main application code here
     example1()
     example2()
-    print("Hello, this is the main application!")
     logger("Hello, this is the main application!")
+    print("Hello, this is the main application!")
 
 if __name__ == "__main__":
+    # Ensure the profiles directory exists
+    os.makedirs('profiles', exist_ok=True)
+
     # Create a profile object
     pr = cProfile.Profile()
 
